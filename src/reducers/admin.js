658e7a8492dec14c62fc75661bbfde5d0afcf068
Jason Hsu <jason80d@gmail.com>
2020-04-24 10:06:44 +0800

    first commit

---
+++ src/reducers/admin.js
@@ -0,0 +1,13 @@
+const initialState = {
+};
+
+const admin = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default admin;
